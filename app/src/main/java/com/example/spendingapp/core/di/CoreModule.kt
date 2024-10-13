60af9ddf1f1a9500da7ae5f874c8783819e41ba0
EbramIbrahim <abramibrahem22@gmail.com>
2024-10-13 14:38:42 +0300

    spending_tracker

--- app/src/main/java/com/example/spendingapp/core/di/CoreModule.kt
+++ app/src/main/java/com/example/spendingapp/core/di/CoreModule.kt
@@ -3,7 +3,13 @@ package com.example.spendingapp.core.di
 import android.content.Context
 import androidx.room.Room
 import com.example.spendingapp.core.data.local.SpendingDataBase
+import com.example.spendingapp.core.data.repository.CoreRepository
+import com.example.spendingapp.core.data.repository.local.SpendingLocalDataSource
+import com.example.spendingapp.core.domain.repository.ICoreRepository
+import com.example.spendingapp.core.domain.repository.local.ISpendingLocalDataSource
 import org.koin.android.ext.koin.androidApplication
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreModule = module {
@@ -23,8 +29,8 @@ val coreModule = module {
             "spending_tracker_preference", Context.MODE_PRIVATE
         )
     }
-
-
+    singleOf(::SpendingLocalDataSource).bind<ISpendingLocalDataSource>()
+    singleOf(::CoreRepository).bind<ICoreRepository>()
 }
 
 
